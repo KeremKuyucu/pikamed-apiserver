26ab577a7e306fc5c67eb350564debaa4d48e9c5
Kerem Kuyucu <84400545+KeremKuyucu@users.noreply.github.com>
2025-08-16 19:38:20 +0300

    update email address

--- app/api/lib/email-utils.ts
+++ app/api/lib/email-utils.ts
@@ -8,7 +8,7 @@ export async function sendEmail(to: string, subject: string, htmlContent: string
       Messages: [
         {
           From: {
-            Email: "pikamed@geogame.can.re",
+            Email: "pikamed@keremkk.com.tr",
             Name: "PikaMed",
           },
           To: [
